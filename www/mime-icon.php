37405308e3b6342c534041222c3e1c1aa9358091
JimmyBear217 <jimmybear217@gmai.com>
2021-01-09 18:39:48 -0800

    added icons and mime+extention handling and associations
    added tables to redirect one mime or extension to a specific mime.

--- www/mime-icon.php
+++ www/mime-icon.php
@@ -25,11 +25,50 @@ if (!empty($_GET["filename"])) {
     $filename = $_GET["filename"];
 }
 
-switch ($mime) {
+// aliases
+$mimeRedirections = array(
+    "application-x-javascript" => "text-x-javascript",
+    "application/x-javascript" => "text-x-javascript",
+    "application/javascript" => "text-x-javascript",
+    "application-x-php" => "text-x-php",
+    "application/x-php" => "text-x-php",
+    "application/php" => "text-x-php",
+    "text-xml" => "application-xml",
+    "text/xml" => "application-xml"
+);
+
+$textRedirections = array(
+    "js" => "javascript"
+);
+
+switch (explode('/', $mime)[0]) {
     case 'directory':
         $mimeIcon = __DIR__ . "/icons/folder-cyan.svg";
         header("Content-Type: image/svg+xml", true, 200);
         echo file_get_contents($mimeIcon);
+        break;
+
+    case 'image':
+        $mimeIcon = __DIR__ . "/icons/image-x-generic.svg";
+        header("Content-Type: image/svg+xml", true, 200);
+        echo file_get_contents($mimeIcon);
+        error_log("Image Mime: $mime for $filename\n", 3, "mimes_not_found.log");
+        break;
+
+    case 'text':
+        $fileExtention = explode('.', $filename);
+        $fileExtention = $fileExtention[count($fileExtention)-1];
+        $mimeIcon = __DIR__ . "/icons/text-x-" .  $fileExtention. ".svg";
+        if (in_array($mimeRedirections, array_keys($mimeRedirections))) {
+            $mimeIcon = __DIR__ . "/icons/" . $mimeRedirections[$mime]. ".svg";
+        } elseif (in_array($fileExtention, $textRedirections)) {
+            $mimeIcon = __DIR__ . "/icons/" . $textRedirections[$fileExtention]. ".svg";
+        } elseif (!file_exists($mimeIcon)) {
+            $mimeIcon = __DIR__ . "/icons/text-x-generic.svg";
+        }
+        header("Content-Type: image/svg+xml", true, 200);
+        echo file_get_contents($mimeIcon);
+        error_log("Text Mime: $mime for $filename with extention $fileExtention\n", 3, "mimes_not_found.log");
         break;
     
     default:
@@ -38,6 +77,10 @@ switch ($mime) {
         if (file_exists($mimeIconOriginal)){
             header("Content-Type: image/svg+xml", true, 200);
             echo file_get_contents($mimeIconOriginal);
+        } elseif (in_array($mimeRedirections, array_keys($mimeRedirections))) {
+            $mimeIcon = __DIR__ . "/icons/" . $mimeRedirections[$mime]. ".svg";
+            header("Content-Type: image/svg+xml", true, 200);
+            echo file_get_contents($mimeIcon);
         } else {
             // check for matching icons is papirus list
             $mimeIcon = __DIR__ . "/icons/papirus-svg/" . str_replace("/", "-", $mime) . ".svg";
